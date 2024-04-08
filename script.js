6b2628e6d4e67e921a9225409e4a6dece4138f90
Lucky Goswami <lgoswami204@gmail.com>
2024-04-08 23:37:37 +0530

    style: zoom slider styled

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const rangeBar = document.getElementById("rangeBar");
 const imageBox = document.querySelector(".imageBox");
 const sceneImg = document.getElementById("sceneImg");
 
+function isDecimal(number) {
+  return number.toString().includes(".");
+}
+
 rangeBar.addEventListener("input", () => {
-  sceneImg.src = `assets/scenes/scene000${rangeBar.value}.webp`;
+  console.log(rangeBar.value);
+  if (!isDecimal(rangeBar.value)) {
+    sceneImg.src = `assets/scenes/scene000${rangeBar.value}.webp`;
+  }
 });
