cf369a7ac962e4c6240df05f78aefb1cdc1a457e
Lucky Goswami <lgoswami204@gmail.com>
2024-04-19 23:01:19 +0530

    rendering head, nav and footer using fetch api to reduce one extra node

--- script.js
+++ script.js
@@ -3,6 +3,51 @@
 const rangeBar = document.getElementById("rangeBar");
 const imageBox = document.querySelector(".imageBox");
 const sceneImg = document.getElementById("sceneImg");
+const head = document.querySelector("head");
+const nav = document.querySelector("nav");
+const footer = document.querySelector("footer");
+
+fetch("components/head.html")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.text();
+  })
+  .then((htmlContent) => {
+    head.innerHTML = htmlContent;
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+
+fetch("components/nav.html")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.text();
+  })
+  .then((htmlContent) => {
+    nav.innerHTML = htmlContent;
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+
+fetch("components/footer.html")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.text();
+  })
+  .then((htmlContent) => {
+    footer.innerHTML = htmlContent;
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
 
 function isDecimal(number) {
   return number.toString().includes(".");
